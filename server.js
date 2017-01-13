334d416e3968563385af3fc41583c2f802630a07
Kai Zau <k@kaizau.com>
2017-01-13 18:43:43 +0400

    WIP actions and custom duration

--- server.js
+++ server.js
@@ -28,45 +28,98 @@ const app = {
   })
 }
 
-slapp.message('^start', ['direct_mention', 'direct_message'], startWorkCycle)
-slapp.command('/work', /^\s*start\s*$/, startWorkCycle)
+const startString = '^\s*start\s*(\d*)'
+slapp.message(startString, ['direct_mention', 'direct_message'], handleStart)
+slapp.command('/work', startString, handleStart)
 
 //
 // Plan Cycle
 //
 
-function startWorkCycle(msg) {
-  msg
-    .say(startResponse)
-    .say(planning[0])
-    // TODO TESTING
-    //.route('planning1')
-    .route('planning4')
+// TODO
+// If passed duration, jump straight into it.
+// If not, show action prompt
+
+function handleStart(msg, text, duration) {
+  console.log(text, duration);
+  msg.say(startResponse)
+
+  if (!duration) {
+    msg.say({
+      text: '',
+      attachments: [
+        {
+          text: 'How long of a cycle?',
+          fallback: 'How long of a cycle?',
+          callback_id: 'handleDuration',
+          actions: [
+            {name: 'answer', text: '30 minutes', type: 'button', value: 30},
+            {name: 'answer', text: '45 minutes',  type: 'button',  value: 45},
+            {name: 'answer', text: '60 minutes', type: 'button', value: 60},
+            {name: 'answer', text: '90 minutes',  type: 'button',  value: 90}
+          ]
+        }
+      ]
+    }).route('handleDuration')
+  } else {
+    startPlanning(msg, duration)
+  }
 }
 
-slapp.route('planning1', msg => {
+slapp.route('handleDuration', msg => {
+  let duration;
+
+  if (msg.type !== 'action') {
+    console.log(msg)
+    return
+  } else {
+    duration = msg.body.actions[0].value
+  }
+
+  msg
+    .respond(msg.body.response_url, {
+      text: '',
+      delete_original: true
+    })
+    .say(`Okay, ${duration} minutes it is.`)
+
+  startPlanning(msg, duration)
+})
+
+function startPlanning(msg, duration) {
+  const state = {duration}
+  console.log('STARTING', state)
+  msg
+    .say(planning[0])
+    .route('planning1', state, 180)
+}
+
+slapp.route('planning1', (msg, state) => {
   msg
     .say(planning[1])
-    .route('planning2')
+    .route('planning2', state, 180)
 })
 
-slapp.route('planning2', msg => {
+slapp.route('planning2', (msg, state) => {
   msg
     .say(planning[2])
-    .route('planning3')
+    .route('planning3', state, 180)
 })
 
-slapp.route('planning3', msg => {
+slapp.route('planning3', (msg, state) => {
   msg
     .say(planning[3])
-    .route('planning4')
+    .route('planning4', state, 180)
 })
 
-slapp.route('planning4', msg => {
+slapp.route('planning4', (msg, state) => {
   msg
     .say(planning[4])
 
-  schedule(msg, 1, 'start_debrief', {}, (error, task) => {
+  schedule(msg, parseInt(state.duration, 10), 'start_debrief', {
+    conversation_id: msg.conversation_id,
+    duration: state.duration
+  }, (error, task) => {
     if (error) { console.log(error) }
   })
 })
@@ -75,38 +128,33 @@ slapp.route('planning4', msg => {
 // Debrief Cycle
 //
 
-// TODO
-// For some reason, routes are not followed from this scheduled
-// event. Perhaps something needs to be passed in the schedule()
-// payload to persist? Alternatively, there could be a default
-// expiration for routes. You'd expect this to start a new route,
-// so perhaps I can ensure that.
 slapp.event('start_debrief', msg => {
-  console.log('> starting debrief!')
+  // Workaround a bug in Slapp Chronos where convo_id is mangled
+  const payload = msg.body.payload
+  msg.conversation_id = payload.conversation_id
+
+  console.log(payload)
   msg
     .say(debrief[0])
-    .route('debrief1')
-
-  console.log('> routed to 1')
+    .route('debrief1', null, 180)
 })
 
 slapp.route('debrief1', msg => {
-  console.log('>> 1!')
   msg
     .say(debrief[1])
-    .route('debrief2')
+    .route('debrief2', null, 180)
 })
 
 slapp.route('debrief2', msg => {
   msg
     .say(debrief[2])
-    .route('debrief3')
+    .route('debrief3', null, 180)
 })
 
 slapp.route('debrief3', msg => {
   msg
     .say(debrief[3])
-    .route('debrief4')
+    .route('debrief4', null, 180)
 })
 
 slapp.route('debrief4', msg => {
